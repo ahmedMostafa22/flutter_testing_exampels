ef83ec5b8fcbbd332aff2349013c67fc881c0c37
Ahmed Mostafa <56844663+ahmedMostafa22@users.noreply.github.com>
2023-08-27 11:33:31 +0300

    Add files via upload

---
+++ widget_testing/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ahmed/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ahmed/AndroidStudioProjects/login_example_testing"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/var/folders/12/dbbdqlgd48v5tjgmbs3mrllc0000gn/T/flutter_tools.2JTbB6/flutter_test_listener.vQwV0z/listener.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,SU5URUdSQVRJT05fVEVTVF9TSE9VTERfUkVQT1JUX1JFU1VMVFNfVE9fTkFUSVZFPWZhbHNl"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/ahmed/AndroidStudioProjects/login_example_testing/.dart_tool/package_config.json"
